2d08f27b59acd84e81bf355cb09c78138baff0c4
Bamba-14 <67744080+Bamba-14@users.noreply.github.com>
2021-05-14 07:25:34 +0200

    Update gamesScript.js

--- gamesScript.js
+++ gamesScript.js
@@ -1,25 +1,25 @@
 var gamesPlayer = document.getElementById("gamesPlayer");
 var gamesBlock = document.getElementById("gamesBlock");
+var counter=0;
 function gamesJump()
 {
-	if(gamesPlayer.classList != "animate")
-	{
-		gamesPlayer.classList.add("animate");
-	}
-	setTimeout(function()
-	{
+	if(gamesPlayer.classList == "animate"){return}
+	gamesPlayer.classList.add("animate");
+	setTimeout(function() {
 		games.gamesPlayer.classList.remove("animate");
 	}, 500);
 }
 
-var checkHit = setInterval(function()
-{
-	var gamesPlayerTop= parseInt(window.getComputedStyle(gamesPlayer).GetPropertyValue("top"));
-	var gamesBlockLeft= parseInt(window.getComputedStyle(gamesBlock).GetPropertyValue("left"));
-	if(gamesBlockLeft<20 && gamesBlockLeft>0 && gamesPlayerTop >=130)
-	{
+var checkHit = setInterval(function() {
+	let gamesPlayerTop = parseInt(window.getComputedStyle(gamesPlayer).getPropertyValue("top"));
+	let gamesBlockLeft = parseInt(window.getComputedStyle(gamesBlock).getPropertyValue("left"));
+	if(gamesBlockLeft<20 && gamesBlockLeft>-20 && gamesPlayerTop >=130) {
 		gamesBlock.style.animation ="none";
-		gamesBlock.style.display = "none";
-		alert("u lose.");
-	}
+		alert("Game Over. score: "+Math.floor(counter/100));
+		counter=0;
+		gamesBlock.style.animation = "block 1s infinite linear";
+		}else{
+			counter++;
+			document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+		}
 }, 10);
