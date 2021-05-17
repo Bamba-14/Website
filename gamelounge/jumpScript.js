9640e5a56a0d356b5eeec9d1789ccab13f931295
Bamba-14 <67744080+Bamba-14@users.noreply.github.com>
2021-05-17 11:48:42 +0200

    Update jumpScript.js

--- gamelounge/jumpScript.js
+++ gamelounge/jumpScript.js
@@ -1,25 +1,23 @@
-var gamesPlayer = document.getElementById("gamesPlayer");
-var gamesBlock = document.getElementById("gamesBlock");
+var character = document.getElementById("character");
+var block = document.getElementById("block");
 var counter=0;
-function gamesJump()
-{
-	if(gamesPlayer.classList == "animate"){return}
-	gamesPlayer.classList.add("animate");
-	setInterval(function() {
-		games.gamesPlayer.classList.remove("animate");
-	}, 1);
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
-
-var checkHit = setInterval(function() {
-	let gamesPlayerTop = parseInt(window.getComputedStyle(gamesPlayer).getPropertyValue("top"));
-	let gamesBlockLeft = parseInt(window.getComputedStyle(gamesBlock).getPropertyValue("left"));
-	if(gamesBlockLeft<20 && gamesBlockLeft>-20 && gamesPlayerTop >=130) {
-		gamesBlock.style.animation ="none";
-		//alert("Game Over. score: "+Math.floor(counter/100));
-		counter=0;
-		gamesBlock.style.animation = "block 1s infinite linear";
-		}else{
-			counter++;
-			//document.getElementById("scoreSpan").innerAdjacentHTML = Math.floor(counter/100);
-		}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
 }, 10);
