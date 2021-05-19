319783af1b349ac08693c3eaa73a415d9590c70d
Bamba-14 <67744080+Bamba-14@users.noreply.github.com>
2021-05-19 08:42:05 +0200

    test leaderboard

--- gamelounge/jumpScript.js
+++ gamelounge/jumpScript.js
@@ -63,3 +63,19 @@ function restart(){
 	currentScoreSpan.classList.remove("inactiveCurrentScore");
 	currentScore.classList.remove("inactiveCurrentScore");
 }
+
+//Load leaderboard
+<script type="text/javascript">
+function LoadFile() {
+    var oFrame = document.getElementById("jumpLeaderboard");
+    var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
+    while (strRawContents.indexOf("\r") >= 0)
+        strRawContents = strRawContents.replace("\r", "");
+    var arrLines = strRawContents.split("\n");
+    alert("File " + oFrame.src + " has " + arrLines.length + " lines");
+    for (var i = 0; i < arrLines.length; i++) {
+        var curLine = arrLines[i];
+        alert("Line #" + (i + 1) + " is: '" + curLine + "'");
+    }
+}
+</script>
