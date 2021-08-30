83838b34ee83c1376bb49f6d46d45af5641eeebc
Bamba-14 <67744080+Bamba-14@users.noreply.github.com>
2021-08-30 11:26:34 +0200

    added second carousel

--- imageScroll.js
+++ imageScroll.js
@@ -1,14 +1,27 @@
-var slideIndex = 0;
-carousel();
+var slideIndex1 = 0;
+var slideIndex2 = 0;
+carousel1();
+carousel2();
 
-function carousel() {
+function carousel1() {
   var i;
   var x = document.getElementsByClassName("slides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
+  slideIndex1++;
+  if (slideIndex1 > x.length) {slideIndex1 = 1}
+  x[slideIndex1-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("slidesJ");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > x.length) {slideIndex2 = 1}
+  x[slideIndex2-1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
