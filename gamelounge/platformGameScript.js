bd1d53e6e9462fe25dca06fa9617c160e7e5a743
Bamba-14 <67744080+Bamba-14@users.noreply.github.com>
2021-08-31 15:17:39 +0200

    updated platform game script

--- gamelounge/platformGameScript.js
+++ gamelounge/platformGameScript.js
@@ -40,10 +40,13 @@ window.onload = function(){
 	context.font = "30px Consolas";
 	context.fillStyle = "white";
 	context.textAlign = "center";
-	context.fillText("Welcome to BuckySide's simple platform game.", canvas.width/2, canvas.height/3);
-	context.fillText("Choose a level above, you can play them in any order", canvas.width/2, canvas.height/2);
-	context.fillText("you want as they're all unlocked.", canvas.width/2, canvas.height/1.8);
-	context.fillText("Use W-A-D to move and jump.", canvas.width/2, canvas.height/1.4);
+	context.fillText("Welcome to BuckySide's simple platform game.", canvas.width/2, canvas.height/6);
+	context.fillText("Choose a level above, I suggest you play them from 1 to 5", canvas.width/2, canvas.height/3);
+	context.fillText("but they're all unlocked.", canvas.width/2, canvas.height/2.6);
+	context.fillText("Use W-A-D to move and jump.", canvas.width/2, canvas.height/2);
+	context.fillText("!!!! -- WARNING -- !!!!", canvas.width/2, canvas.height/1.4);
+	context.fillText("Sometimes you get stuck on the edge, you'll have to", canvas.width/2, canvas.height/1.2);
+	context.fillText("close the tab and open it again", canvas.width/2, canvas.height/1.1);
 
 	//Key listeners
 	setupInputs();
@@ -97,7 +100,7 @@ function Border (x, y, width, height, type) {
 		context.fillRect(this.x, this.y, this.width, this.height);
 	}
 }
-// Chek for intersections
+// Check for intersections
 function checkIntersection(r1, r2){
 	if (r1.x >= r2.x + r2.width){
 		return false;
@@ -119,7 +122,7 @@ function Player(x, y){
 	this.height = 100;
 	this.xvelocity = 0;
 	this.yvelocity = 0;
-	this.friction = 0.1;
+	this.friction = 0.9;
 	this.maxVelocity = 10;
 	this.active = true;
 
@@ -243,7 +246,7 @@ var enemyX = 0;
 var enemyY = 180;
 enemyChangeDirectionX = 0;
 enemyChangeDirectionY = 0;
-var enemyChangeX = 6;
+var enemyChangeX = 2;
 var enemyChangeY = 0;
 var enemyHeight = 10;
 
@@ -254,12 +257,12 @@ function drawEnemy(){
 	context.fill();
 	context.closePath();
 	//X axis
-	if(enemyChangeDirectionX == 1 && enemyChangeX == 6){
-		enemyChangeX = -6;
+	if(enemyChangeDirectionX == 1 && enemyChangeX == 2){
+		enemyChangeX = -2;
 		enemyChangeDirectionX = 0;
 	}
-	if(enemyChangeDirectionX == 1 && enemyChangeX == -6){
-		enemyChangeX = 6;
+	if(enemyChangeDirectionX == 1 && enemyChangeX == -2){
+		enemyChangeX = 2;
 		enemyChangeDirectionX = 0;
 	}
 	enemyX += enemyChangeX;
@@ -267,12 +270,12 @@ function drawEnemy(){
 		enemyChangeDirectionX = 1;
 	}
 	//Y axis
-	if(enemyChangeDirectionY == 1 && enemyChangeY == 6){
-		enemyChangeY = -6;
+	if(enemyChangeDirectionY == 1 && enemyChangeY == 2){
+		enemyChangeY = -2;
 		enemyChangeDirectionY = 0;
 	}
-	if(enemyChangeDirectionY == 1 && enemyChangeY == -6){
-		enemyChangeY = 6;
+	if(enemyChangeDirectionY == 1 && enemyChangeY == -2){
+		enemyChangeY = 2;
 		enemyChangeDirectionY = 0;
 	}
 	enemyY += enemyChangeY;
@@ -417,10 +420,240 @@ function drawLevel2() {
 	borders.push(new Border(400, 420, 600, 10, 2));
 	borders.push(new Border(940, 590 ,130, 10, 2));
 	borders.push(new Border(400, 280, 180, 10, 2));
-	borders.push(new Border(900, 210, 50, 10, 3));
+	borders.push(new Border(900, 110, 50, 10, 3));
 
 	//Enemy
-	makeEnemy = setInterval(drawEnemy, 1000/60);
+	makeEnemy = setInterval(drawEnemy, 1);
+}
+
+//Draw text
+function drawLevelText() {
+	var pause = 50;
+	var chars;
+	context.font = "30px Consolas";
+	context.fillStyle = "white";
+	context.textAlign = "center";
+	count ++;
+	chars = words.substr(0, count);
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.fillText(chars, canvas.width/2, canvas.height/2);
+	if (count < words.length){
+    	setTimeout(drawLevelText, pause);
+	}
+}
+
+// Level 3
+
+function loadLevel3(){
+	//Reset
+	borders = [];
+
+	//Text
+	liveGame = 0;
+	canvas = document.getElementById("gameCanvas");
+	context = canvas.getContext("2d");
+	context.clearRect(0, 0, 1280, 720);
+	words = "Some platforms can dissapear so be careful!"
+	count = 0;
+	drawLevelText();
+	setTimeout(drawLevel3, 4000);
+}
+
+function drawLevel3() {
+	liveGame = 1
+	//Game loop
+	if(loopActive == 0){
+		gameLoop = setInterval(stepLevel, 1000/60);
+		loopActive = 1;
+	}
+
+	//Player
+	mainPlayer = new Player(500, 600);
+
+	//End of canvas borders
+	borders.push(new Border(-10, 0 ,10, 720, 1));
+	borders.push(new Border(-10, 720 ,1280, 10, 1));
+	borders.push(new Border(1280, 0 ,10, 720, 1));
+	borders.push(new Border(-10, -10 ,1280, 10, 1));
+
+	//Borders
+	borders.push(new Border(10, 650 ,150, 10, 2));
+	borders.push(new Border(220, 500, 50, 10, 2));
+	borders.push(new Border(10, 350 ,50, 10, 2));
+	borders.push(new Border(220, 200, 50, 10, 2));
+	borders.push(new Border(320, 150, 50, 10, 2));
+	borders.push(new Border(520, 200, 100, 10, 2));
+	borders.push(new Border(580, 500, 300, 10, 2));
+	borders.push(new Border(1020, 200, 50, 10, 2));
+	borders.push(new Border(1230, 0, 50, 50, 3));
+	borders.push(new Border(820, 200, 100, 10, 4));
+	dissapearingBordersLevel3();
+}
+	
+//Dissapearing borders
+function dissapearingBordersLevel3(){
+	setInterval(function () {
+	borderDisappearLevel3();
+	borderOn = setTimeout(borderAppearLevel3, 2000);
+	}, 4000);
+}
+
+
+//Dissapearing platforms
+function borderAppearLevel3() {
+	borders.push(new Border(820, 200, 100, 10, 4));
+}
+
+function borderDisappearLevel3() {
+	borders.pop();
+}
+
+// Level 4
+
+function loadLevel4(){
+	//Reset
+	borders = [];
+
+	//Text
+	liveGame = 0;
+	canvas = document.getElementById("gameCanvas");
+	context = canvas.getContext("2d");
+	context.clearRect(0, 0, 1280, 720);
+	words = "Combine all of the skills you've gained and finish the level!"
+	count = 0;
+	drawLevelText();
+	setTimeout(drawLevel4, 4000);
+}
+
+function drawLevel4() {
+	liveGame = 1
+	//Game loop
+	if(loopActive == 0){
+		gameLoop = setInterval(stepLevel, 1000/60);
+		loopActive = 1;
+	}
+
+	//Player
+	mainPlayer = new Player(500, 600);
+
+	//End of canvas borders
+	borders.push(new Border(-10, 0 ,10, 720, 1));
+	borders.push(new Border(-10, 720 ,1280, 10, 1));
+	borders.push(new Border(1280, 0 ,10, 720, 1));
+	borders.push(new Border(-10, -10 ,1280, 10, 1));
+
+	//Borders
+	borders.push(new Border(1180, 650 ,100, 10, 2));
+	borders.push(new Border(1080, 500, 100, 10, 2));
+	borders.push(new Border(1180, 350 ,50, 10, 2));
+	borders.push(new Border(1080, 200, 50, 10, 2));
+	borders.push(new Border(920, 150, 100, 10, 2));
+	borders.push(new Border(620, 400, 50, 10, 2));
+	borders.push(new Border(300, 500, 50, 10, 2));
+	borders.push(new Border(200, 400, 50, 10, 2));
+	borders.push(new Border(200, 100, 50, 10, 2));
+	borders.push(new Border(0, 50, 50, 50, 3));
+	borders.push(new Border(325, 250, 50, 10, 4));
+	borders.push(new Border(400, 500, 100, 10, 4));
+	dissapearingBordersLevel4();
+
+	//Enemy
+	makeEnemy = setInterval(drawEnemy, 1);
+}
+	
+//Dissapearing borders
+function dissapearingBordersLevel4(){
+	setInterval(function () {
+	borderDisappearLevel4();
+	borderOn = setTimeout(borderAppearLevel4, 2000);
+	}, 4000);
+}
+
+
+//Dissapearing platforms
+function borderAppearLevel4() {
+	borders.push(new Border(325, 250, 50, 10, 4));
+	borders.push(new Border(400, 500, 100, 10, 4));
+}
+
+function borderDisappearLevel4() {
+	borders.pop();
+	borders.pop();
+}
+
+// Level 5
+
+function loadLevel5(){
+	//Reset
+	borders = [];
+
+	//Text
+	liveGame = 0;
+	canvas = document.getElementById("gameCanvas");
+	context = canvas.getContext("2d");
+	context.clearRect(0, 0, 1280, 720);
+	words = "Good luck with this one."
+	count = 0;
+	drawLevelText();
+	setTimeout(drawLevel5, 2000);
+}
+
+function drawLevel5() {
+	liveGame = 1
+	//Game loop
+	if(loopActive == 0){
+		gameLoop = setInterval(stepLevel, 1000/60);
+		loopActive = 1;
+	}
+
+	//Player
+	mainPlayer = new Player(500, 600);
+
+	//End of canvas borders
+	borders.push(new Border(-10, 0 ,10, 720, 1));
+	borders.push(new Border(-10, 720 ,1280, 10, 1));
+	borders.push(new Border(1280, 0 ,10, 720, 1));
+	borders.push(new Border(-10, -10 ,1280, 10, 1));
+
+	//Borders
+	borders.push(new Border(0, 650 ,20, 10, 2));
+	borders.push(new Border(100, 500, 20, 10, 2));
+	borders.push(new Border(0, 350 ,20, 10, 2));
+	borders.push(new Border(100, 200, 20, 10, 2));
+	borders.push(new Border(0, 150, 20, 10, 2));
+	borders.push(new Border(100, 200, 200, 10, 2));
+	borders.push(new Border(600, 400, 30, 10, 2));
+	borders.push(new Border(700, 300, 30, 10, 2));
+	borders.push(new Border(800, 200, 150, 10, 2));
+	borders.push(new Border(880, 150, 10, 50, 2));
+	borders.push(new Border(1230, 200, 70, 10, 2));
+	borders.push(new Border(1270, 210, 5, 5, 3));
+	borders.push(new Border(400, 300, 100, 10, 4));
+	borders.push(new Border(1100, 400, 30, 10, 4));
+	dissapearingBordersLevel5();
+
+	//Enemy
+	makeEnemy = setInterval(drawEnemy, 1);
+}
+	
+//Dissapearing borders
+function dissapearingBordersLevel5(){
+	setInterval(function () {
+	borderDisappearLevel5();
+	borderOn = setTimeout(borderAppearLevel5, 2000);
+	}, 4000);
+}
+
+
+//Dissapearing platforms
+function borderAppearLevel5() {
+	borders.push(new Border(400, 300, 100, 10, 4));
+	borders.push(new Border(1100, 400, 30, 10, 4));
+}
+
+function borderDisappearLevel5() {
+	borders.pop();
+	borders.pop();
 }
 
 //Draw text
@@ -446,6 +679,8 @@ function stepLevel(){
 	//Step player
 	if(liveGame == 1){
 		mainPlayer.step();
+		//Dissapearing borders
+		//dissapearingBorders();
 		//Draw everything
 		draw();
 	}
